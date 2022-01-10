8505319e33128e169c57e16351b95c76d528dea0
Billy Mott <billygm@uw.edu>
2022-01-10 13:32:00 -0800

    adding license, and a starting file that got missed in the initial copy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # README-Generator
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
 A command line app that dynamically generates a professional README.md file from a user's input.
--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+
+    }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            console.log(answers);
+        })
+}
 
 // Function call to initialize app
 init();
